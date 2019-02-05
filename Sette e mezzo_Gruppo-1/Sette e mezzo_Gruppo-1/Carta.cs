e1d1c0eb03d134777f6efab2a31a7a794a3a529f
Lucio1590 <luciandaiconu123@gmail.com>
2019-02-05 12:19:39 +0100

    carata

--- Sette e mezzo_Gruppo-1/Sette e mezzo_Gruppo-1/Carta.cs
+++ Sette e mezzo_Gruppo-1/Sette e mezzo_Gruppo-1/Carta.cs
@@ -10,15 +10,18 @@ namespace Sette_e_mezzo_Gruppo_1
     {
         private int _valore;
         private Segno _segno;
-        private string _path;
+        private string _percorsoImmagineCarta;
+
+        public string PercorsoImmagineCarta { get => _percorsoImmagineCarta; set => _percorsoImmagineCarta = value; }
+
         public enum Segno { c, b, d, s };
 
 
         public Carta(int v, Segno s, string path)
         {
             _valore = v;
-            _segno = s;//cia0
-            _path = path;
+            _segno = s;
+            PercorsoImmagineCarta = path;
         }
     }
 }
